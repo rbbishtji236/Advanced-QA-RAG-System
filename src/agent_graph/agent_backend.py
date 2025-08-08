99dca69abc487bf73abb08bc0428b909c6939063
Rohit Bisht <rbbishtji236@gmail.com>
2025-08-09 01:42:45 +0530

    create agent file

---
+++ src/agent_graph/agent_backend.py
@@ -0,0 +1,61 @@
+import json
+from IPython.display import Image, display
+from typing import Annotated, Literal
+from typing_extensions import TypedDict
+from langchain_core.messages import ToolMessage
+from langgraph.graph.message import add_messages
+
+class State(TypedDict):
+    """Represents the state structure containing a list of messages.
+
+    Attributes:
+        messages (list): A list of messages, where each message can be processed
+        by adding messages using the `add_messages` function.
+    """
+    messages:Annotated[list,add_messages]
+
+class BasicToolNode:
+    def __init__(self,tools:list)->None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self,inputs:dict):
+        if messages := inputs.get("messages",[]):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs=[]
+        for tool_call in message.tool_calls:
+            tool_result=self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
+    
+def route_tools(
+    state: State,
+) -> Literal["tools", "__end__"]:
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(
+            f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return "__end__"
+
+
+def plot_agent_schema(graph):
+    try:
+        display(Image(graph.get_graph().draw_mermaid_png()))
+    except Exception:
+        # This requires some extra dependencies and is optional
+        return print("Graph could not be displayed.")
